11f68e43a0d17995d23bb88f25c591804415aa22
Monteiro <rms_monteirosteil@hotmail.com>
2025-08-16 22:28:50 -0300

    Subindo classes request de categorias

---
+++ app/Http/Requests/DestroyCategoriaRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+use App\Models\Categoria;
+
+
+class DestroyCategoriaRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return auth()->user()->is_admin;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+           'id' =>[
+               'required',
+               'integer',
+                Rule::exists('categorias', 'id')->whereNull('deleted_at'),
+
+           ] 
+        ];
+    }
+}
+
