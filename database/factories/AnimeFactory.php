7b4dda584f1e5c48c548a27bb1bfd7407b06ce0f
Monteiro <rms_monteirosteil@hotmail.com>
2025-08-23 10:09:05 -0300

    Implementando anime factory

---
+++ database/factories/AnimeFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Animes>
+ */
+class AnimeFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'titulo' => $this->faker->word(),
+            'ano_lancamento' => $this->faker->year(),
+            'sinopse' => $this->faker->sentence(),
+            'status' => $this->faker->word(),
+            'url_imagem' => $this->faker->sentence(),
+            'genero' => $this->faker->word(),
+        ];
+    }
+}
