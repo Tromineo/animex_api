c291d356a47e922678bf75ea1ffdb8a795441c89
Monteiro <rms_monteirosteil@hotmail.com>
2025-08-22 21:19:35 -0300

    Subindo novas migration

---
+++ database/migrations/2025_08_23_000957_create_animes_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('animes', function (Blueprint $table) {
+            $table->id();
+            $table->string('titulo');
+            $table->string('sinopse');
+            $table->integer('ano_lancamento');
+            $table->string('url_imagem');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('animes');
+    }
+};
