0e4dcad35eb8aba1822ef1fd2ac145b33596ba7c
Monteiro <rms_monteirosteil@hotmail.com>
2025-09-08 19:21:12 -0300

    Corrigindo model de favoritos

--- app/Models/Favoritos.php
+++ app/Models/Favoritos.php
@@ -3,8 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Favoritos extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = "favorites";
+
+    protected $fillable = [
+        'user_id',
+        'anime_id',
+    ];
 }
