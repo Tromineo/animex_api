9513a43b7a3e99261eb1f4c411b54e3663c95251
Monteiro <rms_monteirosteil@hotmail.com>
2025-09-09 19:44:32 -0300

    Subindo factory de favoritos

---
+++ database/factories/FavoritosFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Favoritos>
+ */
+class FavoritosFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'anime_id' => $this->faker->numberBetween(51, 100),
+            'user_id' => 1,
+        ];
+    }
+}
