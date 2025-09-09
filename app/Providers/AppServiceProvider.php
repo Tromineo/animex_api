6eb39f42329abefaa60c36e798a70c5844dad2f1
Monteiro <rms_monteirosteil@hotmail.com>
2025-09-09 19:38:32 -0300

    Atualizando composer.json

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,13 +3,16 @@
 namespace App\Providers;
 
 use App\Models\Anime;
+use App\Models\Favoritos;
 use App\Policies\AnimePolicy;
+use App\Policies\FavoritosPolicy;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
     protected $policies = [
-        Anime::class => AnimePolicy::class
+        Anime::class => AnimePolicy::class,
+        Favoritos::class => FavoritosPolicy::class
     ];
     /**
      * Register any application services.
