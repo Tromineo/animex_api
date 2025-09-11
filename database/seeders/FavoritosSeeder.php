771f7cf591b2e4d6ad0c443dceaf5e184b8d121d
Monteiro <rms_monteirosteil@hotmail.com>
2025-09-11 20:58:32 -0300

    Configurando seeders de favoritos

--- database/seeders/FavoritosSeeder.php
+++ database/seeders/FavoritosSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Favoritos;
 
 class FavoritosSeeder extends Seeder
 {
@@ -12,6 +13,6 @@ class FavoritosSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Favoritos::factory(50)->create();
     }
 }
