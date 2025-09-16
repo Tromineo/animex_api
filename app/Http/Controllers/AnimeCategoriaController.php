f41dcc1a255e71546445f21208f020e7ec5f9e47
Monteiro <rms_monteirosteil@hotmail.com>
2025-09-15 22:47:16 -0300

    Atualizando phpdoc do metodo show de animecategoria

--- app/Http/Controllers/AnimeCategoriaController.php
+++ app/Http/Controllers/AnimeCategoriaController.php
@@ -29,6 +29,38 @@ class AnimeCategoriaController extends Controller
         return response()->json($categorias, Response::HTTP_OK);
     }
 
+/**
+ * @OA\Get(
+ *     path="/anime-categorias/{id}",
+ *     summary="Exibir um anime-categoria pelo ID",
+ *     description="Retorna um objeto de anime-categoria pelo seu ID",
+ *     operationId="showAnimeCategoria",
+ *     tags={"AnimeCategorias"},
+ *     @OA\Parameter(
+ *         name="id",
+ *         in="path",
+ *         required=true,
+ *         description="ID do anime-categoria",
+ *         @OA\Schema(type="integer", example=1)
+ *     ),
+ *     @OA\Response(
+ *         response=200,
+ *         description="Anime-categoria encontrado",
+ *         @OA\JsonContent(
+ *             type="object",
+ *             example={"id": 1, "anime_id": 10, "categoria_id": 2}
+ *         )
+ *     ),
+ *     @OA\Response(
+ *         response=404,
+ *         description="Anime-categoria não encontrado",
+ *         @OA\JsonContent(
+ *             type="object",
+ *             example={"message": "Anime-categoria nao encontrado"}
+ *         )
+ *     )
+ * )
+ */
     public function show(int $id): JsonResponse
     {
         try {
@@ -37,5 +69,6 @@ class AnimeCategoriaController extends Controller
             return response()->json(['message' => 'Anime-categoria nao encontrado'], Response::HTTP_NOT_FOUND);
         }
     }
+
 
 }
