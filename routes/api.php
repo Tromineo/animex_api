9ea39e4f204e2d05e88f5df9e4b6f2cf06447063
rodrigo <rms_monteirosteil@hotmail.com>
2026-03-31 12:38:25 -0300

    Incluindo throttle rotas de login

--- routes/api.php
+++ routes/api.php
@@ -12,10 +12,10 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register'])->middleware('throttle:register');
+Route::post('/login', [AuthController::class, 'login'])->middleware('throttle:login');
 
-Route::middleware('auth:sanctum')->group(function () {
+Route::middleware(['auth:sanctum', 'throttle:api'])->group(function () {
 
     Route::group(['prefix' => 'animes'], function () {
         Route::get('/', [AnimeController::class, 'index']);
